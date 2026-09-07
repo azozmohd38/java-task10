cff5922d6a819891671c2577a3fa3d71f848124f
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:35:13 +0400

    clean task395 code

--- untitled/src/Task395.java
+++ untitled/src/Task395.java
@@ -1,16 +1,11 @@
 public class Task395 {
     public static boolean contains(String str, char target) {
-        // Base case: empty string
         if (str.isEmpty()) {
             return false;
         }
-
-        // Early exit if match found
         if (str.charAt(0) == target) {
             return true;
         }
-
-        // Check remainder of the string
         return contains(str.substring(1), target);
     }
 
