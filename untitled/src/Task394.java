ace150754c85409e55ab9571bd212dc72bd34363
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:34:50 +0400

    rename task394 variable

--- untitled/src/Task394.java
+++ untitled/src/Task394.java
@@ -1,18 +1,18 @@
 public class Task394 {
-    public static int countZeros(int num) {
-        if (num == 0) {
+    public static int countZeros(int number) {
+        if (number == 0) {
             return 1;
         }
-        return countZerosHelper(Math.abs(num));
+        return countZerosHelper(Math.abs(number));
     }
 
-    private static int countZerosHelper(int num) {
-        if (num == 0) {
+    private static int countZerosHelper(int number) {
+        if (number == 0) {
             return 0;
         }
 
-        int count = (num % 10 == 0) ? 1 : 0;
-        return count + countZerosHelper(num / 10);
+        int count = (number % 10 == 0) ? 1 : 0;
+        return count + countZerosHelper(number / 10);
     }
 
     public static void main(String[] args) {
