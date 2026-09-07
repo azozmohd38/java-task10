ca0f1e5b61c822d79e579b6771840e9d65dc1c06
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:37:12 +0400

    fix task397 variable rename

--- untitled/src/Task397.java
+++ untitled/src/Task397.java
@@ -1,15 +1,21 @@
 public class Task397 {
-    public static Strinumberg toBinumberary(inumbert number) {
-        if (number == 0) returnumber "0";
-        if (number == 1) returnumber "1";
+    public static String toBinary(int number) {
+        if (number == 0 || number == 1) {
+            return String.valueOf(number);
+        }
 
-        returnumber toBinumberary(number / 2) + (number % 2);
+        String previous = toBinary(number / 2);
+        return previous + (number % 2);
     }
 
-    public static void mainumber(Strinumberg[] args) {
-        IO.prinumbertlnumber(toBinumberary(2));
-        IO.prinumbertlnumber(toBinumberary(5));
-        IO.prinumbertlnumber(toBinumberary(10));
-        IO.prinumbertlnumber(toBinumberary(1));
+    private static void runExamples() {
+        System.out.println(toBinary(2));
+        System.out.println(toBinary(5));
+        System.out.println(toBinary(10));
+        System.out.println(toBinary(1));
+    }
+
+    public static void main(String[] args) {
+        runExamples();
     }
 }
