a3c77bacfadeda8c89c923868e821db3dad8311a
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:35:57 +0400

    clean task396 code

--- untitled/src/Task396.java
+++ untitled/src/Task396.java
@@ -4,19 +4,16 @@ public class Task396 {
     }
 
     private static int sumEvenIndexHelper(int[] arr, int index) {
-        // Base case: index past array bounds
         if (index >= arr.length) {
             return 0;
         }
-
-        // Add current element + recurse at index + 2
         return arr[index] + sumEvenIndexHelper(arr, index + 2);
     }
 
     public static void main(String[] args) {
-        IO.println(sumEvenIndex(new int[]{10, 5, 20, 5, 30})); // 60
-        IO.println(sumEvenIndex(new int[]{1, 2, 3, 4}));        // 4
-        IO.println(sumEvenIndex(new int[]{7}));                 // 7
-        IO.println(sumEvenIndex(new int[]{2, 9}));              // 2
+        IO.println(sumEvenIndex(new int[]{10, 5, 20, 5, 30}));
+        IO.println(sumEvenIndex(new int[]{1, 2, 3, 4}));
+        IO.println(sumEvenIndex(new int[]{7}));
+        IO.println(sumEvenIndex(new int[]{2, 9}));
     }
 }
