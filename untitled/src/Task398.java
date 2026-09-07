cfb4bd25b6ba2cc55c4dace500ef528b9b8b4dba
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:37:27 +0400

    rename task398 variable

--- untitled/src/Task398.java
+++ untitled/src/Task398.java
@@ -1,12 +1,12 @@
 public class Task398 {
-    public static int countUpper(String str) {
-        if (str.isEmpty()) {
+    public static int countUpper(String text) {
+        if (text.isEmpty()) {
             return 0;
         }
 
-        int count = Character.isUpperCase(str.charAt(0)) ? 1 : 0;
+        int count = Character.isUpperCase(text.charAt(0)) ? 1 : 0;
 
-        return count + countUpper(str.substring(1));
+        return count + countUpper(text.subtexting(1));
     }
 
     public static void main(String[] args) {
